4f2581749bded5052612207c450f4659321bf95e
Davide Leone <it.davide.leone@gmail.com>
2024-01-25 01:17:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Description
+This script has been developed as part of a university project for UniBA - Università degli Studi di Bari Aldo Moro. It captures and analyzes network traffic during the download process. Additionally, the program extracts metadata associated with the video and retrieves the certificate information for the given URL.
+
+## Prerequisites
+
+- **Python Installation:** Ensure that [Python](https://www.python.org/downloads/) is installed on your system.
+
+- **FFmpeg Installation:**
+  - **Mac:** You can install FFmpeg using Homebrew with the following command:
+    ```bash
+    brew install ffmpeg
+    ```
+  - **Windows:** Download FFmpeg from the official site [ffmpeg.org](https://ffmpeg.org/download.html) and add it to the system's environment path.
+
+- **Dependencies:** Install the dependencies using the requirements.txt
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+- **Running the script:** To utilize the script, follow these commands and enter the prompted information:
+   ```bash
+   git clone https://github.com/daveleone/video_extractor.git
+   ```
+   ```bash
+   cd video_extractor
+   ```
+   ```bash
+   python main.py
+   ```
---
+++ main.py
@@ -0,0 +1,152 @@
+import logging, warnings
+
+# Suppressing pandas deprecation warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+import pandas as pd
+from pandas import DataFrame
+
+# Suppressing scapy warnings
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import sniff, Packet
+from scapy.layers.inet import TCP, UDP, IP
+
+import openpyxl, json, requests, ssl, certifi
+from yt_dlp import YoutubeDL
+from pydub.utils import mediainfo
+from typing import Union, Dict
+from urllib.parse import urlparse
+
+
+YTDL_OPTIONS = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(title)s.%(ext)s",
+    # 'postprocessors': [
+    #     {
+    #         'key': 'FFmpegVideoConvertor',
+    #         'preferedformat': 'mp4',
+    #     }
+    # ],
+}
+
+def extract_video(options: dict, url: str) -> str:
+    try:
+        with YoutubeDL(options) as ytdl:
+            info_dict = ytdl.extract_info(url, download = False)
+            file_name = info_dict["title"]
+            
+            sniff(iface = "en0", prn = lambda x: packet_handler(x, url, file_name, packet_data), count=1)
+
+            ytdl.download([url])
+    except Exception as e:
+        print(f"Error during download: {e}")
+        return None
+    
+    return file_name
+
+def packet_handler(packet: Packet, url: str, file_name: str, packet_data: list) -> None:
+    source_ip, destination_ip, source_port, destination_port, flags = None, None, None, None, None
+    protocol, length, payload = None, None, None
+
+    if IP in packet:
+        source_ip = packet[IP].src
+        destination_ip = packet[IP].dst
+        protocol = packet[IP].proto
+        length = len(packet)
+
+        if TCP in packet:
+            source_port = packet[TCP].sport
+            destination_port = packet[TCP].dport
+            flags = packet[TCP].flags
+            payload = str(packet[TCP].payload)
+        elif UDP in packet:
+            source_port = packet[UDP].sport
+            destination_port = packet[UDP].dport
+            payload = str(packet[UDP].payload)
+
+    packet_data.append({
+        "Video Downloaded": file_name,
+        "Length": length,
+        "Source IP": source_ip,
+        "Destination IP": destination_ip,
+        "Source Port": source_port,
+        "Destination Port": destination_port,
+        "Protocol": protocol,
+        "Flags": flags,
+        "Payload": payload,
+    })
+
+def excel_writer(data_frame: DataFrame, sheet_path: str) -> None:
+    with pd.ExcelWriter("packet_information.xlsx", engine = "openpyxl", mode = "a", if_sheet_exists = "overlay") as writer:
+            data_frame.to_excel(writer, sheet_name = "Sheet1", startrow = writer.sheets["Sheet1"].max_row, index = False, header = False)
+            worksheet = writer.sheets["Sheet1"]
+            for column in worksheet.columns:
+                max_length = max(len(str(cell.value)) for cell in column)
+                worksheet.column_dimensions[column[0].column_letter].width = max_length + 2
+
+def convert_to_excel(packet: dict) -> None:
+    data_frame = pd.DataFrame(packet)
+
+    if not pd.io.common.file_exists("packet_information.xlsx"):
+        data_frame.to_excel("packet_information.xlsx", sheet_name = "Sheet1", index = False)
+        print("Network traffic analyzed and stored!")
+        return None
+
+    try:
+        excel_writer(data_frame, "packet_information.xlsx")
+        print("Network traffic analyzed and stored!")
+    except Exception as e:
+        print(f"Error during excel creation: {e}")
+        return None
+
+def extract_metadata(file_path: str) -> Union[Dict[str, str], None]:
+    try:
+        metadata = mediainfo(file_path)
+        return metadata
+    except Exception as e:
+        print(f"Error extracting metadata: {e}")
+        return None
+    
+def convert_metadata_json(video_name: str, metadata: Dict[str, str]) -> None:
+    try:
+        json_file_name = f"{video_name}_metadata.json"
+        with open(json_file_name, "w") as json_file:
+            json.dump(metadata, json_file, indent=2)
+        print(f"Metadata saved to {json_file_name}")
+    except Exception as e:
+        print(f"Error saving metadata to JSON file: {e}")
+        return None
+
+def extract_domain_name(url: str) -> str:
+    domain = urlparse(url).netloc
+
+    return domain
+
+def extract_certificate(url: str, domain_name: str) -> None:
+    response = requests.get(url)
+    try:
+        certificate = ssl.get_server_certificate((urlparse(url).hostname, 443), ca_certs=certifi.where())
+
+        output_file = f"{domain_name}_certificate.pem"
+        with open(output_file, 'w') as pem_file:
+            pem_file.write(certificate)
+
+        print(f"Certificate saved to {output_file}")
+    except Exception as e:
+        print(f"Error extracting certificate {e}: ")
+        return None
+
+
+
+if __name__ == "__main__":
+    packet_data = []
+
+    url = input("Enter the URL of the video you want to download: ")
+    video_name = extract_video(YTDL_OPTIONS, url)
+
+    convert_to_excel(packet_data)
+    
+    file_path = input("Enter the path to the video from which you want to extract metadata: ")
+    metadata = extract_metadata(file_path)
+    convert_metadata_json(video_name, metadata)
+
+    extract_certificate(url, extract_domain_name(url))
